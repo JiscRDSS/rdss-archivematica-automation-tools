10c8c177496304fbe1a8e10151e983da4ade6309
Jeremy Sparks <jeremysparks86@gmail.com>
2017-08-03 16:22:47 +0100

    Added the volume for the automation config XML files

--- automation-tools/setup.sh
+++ automation-tools/setup.sh
@@ -10,6 +10,10 @@ cp /usr/lib/archivematica/automation-tools/etc/transfer-script.sh /etc/archivema
 mkdir -p /var/log/archivematica/automation-tools
 mkdir -p /var/archivematica/automation-tools
 
+# Set up XML config files to define behaviour
+cp /var/archivematica/sharedDirectory/sharedMicroServiceTasksConfigs/processingMCPConfigs/defaultProcessingMCP.xml /usr/lib/archivematica/automation-tools/transfers/pre-transfer/
+cp /usr/lib/archivematica/automation-tools/transfers/examples/pre-transfer/default_config.py /usr/lib/archivematica/automation-tools/transfers/pre-transfer/
+
 # Set up transfer script
 chmod +x /etc/archivematica/automation-tools/transfer-script.sh
 
@@ -18,3 +22,4 @@ while true; do
     sh /etc/archivematica/automation-tools/transfer-script.sh;
     sleep 120
 done
+
