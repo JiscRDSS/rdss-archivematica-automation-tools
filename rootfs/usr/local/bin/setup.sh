d7295acb4968be248d6218b6390b303c26d86ce1
Richard Lowe <richard.lowe@arkivum.com>
2017-08-17 16:40:26 +0100

    Ensure that copied processing XML is not executable in setup.sh

--- rootfs/usr/local/bin/setup.sh
+++ rootfs/usr/local/bin/setup.sh
@@ -19,5 +19,6 @@ mkdir -p "/var/archivematica/automation-tools"
 # Set up XML config files to define behaviour
 cp "${AM_PROCESSING_CONF}/defaultProcessingMCP.xml" \
     "${AM_TOOLS_HOME}/transfers/pre-transfer/"
+chmod -x "${AM_TOOLS_HOME}/transfers/pre-transfer/defaultProcessingMCP.xml"
 cp "${AM_TOOLS_HOME}/transfers/examples/pre-transfer/default_config.py" \
     "${AM_TOOLS_HOME}/transfers/pre-transfer/"
