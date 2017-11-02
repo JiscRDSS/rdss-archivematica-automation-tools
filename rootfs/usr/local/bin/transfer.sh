c787ba4104d63c7754abb0548262092018f1c98e
Richard Lowe <richard.lowe@arkivum.com>
2017-11-02 11:28:21 +0000

    shell scripts: fix linting issues
    These were picked up by ShellCheck as part of Travis CI build.

--- rootfs/usr/local/bin/transfer.sh
+++ rootfs/usr/local/bin/transfer.sh
@@ -16,17 +16,17 @@ SS_URL="${AM_TOOLS_TRANSFER_SS_URL:-http://127.0.0.1:8000}"
 #
 transfer_source_id=$(curl -k -s \
     -H "Authorization: ApiKey ${AM_TOOLS_TRANSFER_SS_USER}:${AM_TOOLS_TRANSFER_SS_API_KEY}" \
-        ${SS_URL}/api/v2/location/ | \
+        "${SS_URL}/api/v2/location/" | \
     jq -r ".objects[] | select(.purpose == \"TS\" and .description == \"${AM_TOOLS_TRANSFER_SOURCE_DESCRIPTION}\") | .uuid")
 
-if [ "${transfer_source_id}" == "" ] ; then
+if [ "${transfer_source_id}" = "" ] ; then
     >&2 echo "No transfer source found matching description '${AM_TOOLS_TRANSFER_SOURCE_DESCRIPTION}'"
     exit 1
 fi
 echo "Using transfer source id '${transfer_source_id}'"
 
 # Found the transfer source, do the transfer
-cd /usr/lib/archivematica/automation-tools/
+cd /usr/lib/archivematica/automation-tools/ && \
 python -m transfers.transfer \
   --am-url "${AM_TOOLS_TRANSFER_AM_URL:-http://127.0.0.1}" \
   --api-key "${AM_TOOLS_TRANSFER_AM_API_KEY}" \
